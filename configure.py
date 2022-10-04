ab19d31d9db584c32b7cdbf1c298ee118419753a
Filatova Marina <uhlovskaya.filatovamn@yandex.ru>
2022-10-04 19:43:21 +0300

    Create configure.py

---
+++ configure.py
@@ -0,0 +1,10 @@
+TOKEN = '5544284197:AAELygnH0xis7XdKVxwa0RpXInpU482AK3M'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUR',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH'
+
+}
